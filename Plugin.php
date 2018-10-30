d0da6d955a15dbb9e909e3d252e47aa2129abacb
Valentino Pesce <valentino@iltuobrand.it>
2018-10-30 19:57:35 +0100

    update compatible version

--- Plugin.php
+++ Plugin.php
@@ -60,7 +60,7 @@ class Plugin extends Base
 
     public function getCompatibleVersion()
     {
-        return '>=1.2.5';
+        return '>=1.0.48';
     }
 
     public function getPluginHomepage()
