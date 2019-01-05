01fb6772f7c9472cf92a138c749cfc5b6b36b546
Valentino Pesce <valentino@iltuobrand.it>
2019-01-05 11:55:19 +0100

    Fix: logo not clickable

--- Template/layout/header/title.php
+++ Template/layout/header/title.php
@@ -1,20 +1,20 @@
 <?php global $themeNebulaConfig; ?>
-<div class="header">
-    <?php if (!isset($themeNebulaConfig['logo'])) : ?>
-        <?= $this->url->link('K<span>B</span>', 'DashboardController', 'show', array(), false, '', t('Dashboard')) ?>
-    <?php else: ?>
-        <?= $this->url->link('<img src="'.$this->url->dir().'plugins/Nebula/Assets/images/'.$themeNebulaConfig['logo'].'" />', 'DashboardController', 'show', array(), false, '', t('Dashboard')) ?>
-    <?php endif ?>
-    <h2>
+    <span class="logo">
+        <?php if (!isset($themeNebulaConfig['logo'])) : ?>
+            <?= $this->url->link('K<span>B</span>', 'DashboardController', 'show', array(), false, '', t('Dashboard')) ?>
+        <?php else: ?>
+            <?= $this->url->link('<img src="'.$this->url->dir().'plugins/Nebula/Assets/images/'.$themeNebulaConfig['logo'].'" style="float: left;max-height: 40px;margin-top: -7px;margin-right: 10px;" />', 'DashboardController', 'show', array(), false, '', t('Dashboard')) ?>
+        <?php endif ?>
+    </span>
+<h1>
+    <span class="title">
         <?php if (! empty($project) && ! empty($task)): ?>
             <?= $this->url->link($this->text->e($project['name']), 'BoardViewController', 'show', array('project_id' => $project['id'])) ?>
         <?php else: ?>
             <?= $this->text->e($title) ?>
         <?php endif ?>
-        <small>
-            <?php if (! empty($description)): ?>
-                <?= $this->app->tooltipHTML($description) ?>
-            <?php endif ?>
-        </small>
-    </h2>
-</div>
+    </span>
+    <?php if (! empty($description)): ?>
+        <?= $this->app->tooltipHTML($description) ?>
+    <?php endif ?>
+</h1>
