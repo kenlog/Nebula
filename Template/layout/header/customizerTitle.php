7e8a3a90fb020c6d0aa4a067b40cf9f5e4133ff9
Valentino Pesce <valentino@iltuobrand.it>
2019-01-09 22:58:58 +0100

    fix logo link Customizer

--- Template/layout/header/customizerTitle.php
+++ Template/layout/header/customizerTitle.php
@@ -1,19 +1,17 @@
-<div class="header">
-		<?php if (null !== $this->task->customizerFileModel->getByType(1)) : ?>
-            <a href="<?= $this->url->href('DashboardController', 'show', array(), false, '', t('Dashboard')) ?>">
-                <img src="<?= $this->url->href('CustomizerFileController', 'image', array('plugin' => 'customizer', 'file_id' => $this->task->customizerFileModel->getIdByType(1))) ?>" height="50">
-            </a>
-            <h2>
-                <?php if (! empty($project) && ! empty($task)): ?>
-                    <?= $this->url->link($this->text->e($project['name']), 'BoardViewController', 'show', array('project_id' => $project['id'])) ?>
-                <?php else: ?>
-                    <?= $this->text->e($title) ?>
-                <?php endif ?>
-                <small>
-                    <?php if (! empty($description)): ?>
-                        <?= $this->app->tooltipHTML($description) ?>
-                    <?php endif ?>
-                </small>
-            </h2>
-		<?php endif ?>
-</div>
+    <span class="logo">
+        <?php if (null !== $this->task->customizerFileModel->getByType(1)) : ?>
+            <?= $this->url->link('<img src="'.$this->url->href("CustomizerFileController", "image", array("plugin" => "customizer", "file_id" => $this->task->customizerFileModel->getIdByType(1))).'" style="float: left;max-height: 40px;margin-top: -7px;margin-right: 10px;" >', 'DashboardController', 'show', array(), false, '', t('Dashboard')) ?>
+        <?php endif ?>
+    </span>
+    <h1>
+        <span class="title">
+            <?php if (! empty($project) && ! empty($task)): ?>
+                <?= $this->url->link($this->text->e($project['name']), 'BoardViewController', 'show', array('project_id' => $project['id'])) ?>
+            <?php else: ?>
+                <?= $this->text->e($title) ?>
+            <?php endif ?>
+        </span>
+        <?php if (! empty($description)): ?>
+            <?= $this->app->tooltipHTML($description) ?>
+        <?php endif ?>
+    </h1>
